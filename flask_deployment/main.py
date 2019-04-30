02e39954df7bdb7edd02959b2a02e7c17d8ee8f7
DivineUnited <DanielDukeVu@outlook.com>
2019-04-30 11:11:13 +0800

    flask deployment

--- flask_deployment/main.py
+++ flask_deployment/main.py
@@ -2,37 +2,47 @@ from flask import Flask, render_template, request
 from keras.models import load_model
 import tensorflow as tf
 import numpy as np
-import flask
+# import flask
 import re
+from keras.preprocessing import image
+from skimage import transform
 
 app=Flask(__name__)
 
 # our ML model should be saved in the same folder as our main.py
 loaded_model = load_model("model_malaria.h5")
+# prevent tensorflow error caused by running the predict function in a separate thread (which happens when routing).
+loaded_model._make_predict_function()
+graph = tf.get_default_graph()
 
 def ValuePredictor(np_arr):   
-    result = loaded_model.predict(np_arr)
+    global graph
+    # opening up the graph above and managing the resources "with"
+    with graph.as_default():
+        result = loaded_model.predict(np_arr)
     return result[0]
 
-def string_to_int(s):
-    integer = re.sub('[^0-9]','', s)
-    return int(integer)
+def image_preprocess(img):
+    # setting the new shape of image to what we trained our model on
+    new_shape = (50, 50, 3)
 
-def one_hot_input(s, length):
-    index = string_to_int(s)
-    arr = [0 for i in range(length)]
-    arr[index] = 1
-    return arr
+    # Load the image from disk
+    img = image.load_img(img)
 
-def flatten_arr(arr):
-    flat_list = []
-    for item in arr:
-        if isinstance(item, (list)):
-            for it in item:
-                flat_list.append(it)
-        else:
-            flat_list.append(item)
-    return flat_list
+    # Convert the image to a numpy array
+    image_array = image.img_to_array(img)
+
+    # resize the image (must be done after it has turned into a np array):
+    image_array = transform.resize(image_array, new_shape, anti_aliasing=True)
+
+    # scaling the image data to fall between 0-1 since images have 255 brightness values:
+    image_array /= 255
+
+    # The input shape we have defined is the shape of a single sample. The model itself expects some array of samples as input (even if its an array of length 1). Your output really should be 4-d, with the 1st dimension to enumerate the samples. i.e. for a single image you should return a shape of (1, 50, 50, 3).
+    # add an extra dimension
+    image_array = np.expand_dims(image_array, axis=0)
+
+    return image_array
 
 
 @app.route('/')
@@ -45,32 +55,29 @@ def result():
     prediction=''
     if request.method == 'POST':
         # from the request form, convert it to a dictionary saved as this variable
-        features = request.form.to_list()
+        img = request.files['pic']
 
-        prediction = features
+        # pre-process the image that was posted to us
+        img_arr = image_preprocess(img)
 
-        return render_template("result.html",prediction=prediction)
-
-        '''
-        # get the values and turn it into a list
-        features=list(features.values())
-        # flatten the list:
-        features = flatten_arr(features)
-        # reshape the list into a np array: (with unknown amount of columns)
-        features = np.array(features).reshape(1,-1)
-        print("Before sending to model", features) # flag
-        # sending to our prediction model (which will reshape it as a numpy array and then return our result)
-        result = ValuePredictor(features)
+        # sending to our prediction model
+        result = ValuePredictor(img_arr)
         print("result from model", result) # flag
-        if int(result)==0:
-            prediction='Statistics say you will make LESS than $50,000 USD per year.'
+
+        # most likely class index:
+        result = int(np.argmax(result))
+
+        print("result actual", result) # flag
+
+        if result==0:
+            prediction='This cell is likely NOT INFECTED with the Malaria Parasite.'
         else:
-            prediction='Statistics say you will make MORE than $50,000 USD per year.'
+            prediction='This cell is likely INFECTED with the Malaria Parasite.'
+
         print(prediction) # flag
         # passing the string of our prediction to our template
-        return render_template("result.html",prediction=prediction)
-        '''
-        
+        return render_template("result.html", prediction=prediction)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
